d619e9946b81ece4e6cb41d66d61a35e5fd7a67a
Oriol Piera <oriol.piera@somenergia.coop>
2026-07-13 16:04:06 +0200

    fix: remove id_default_combination from product PUT XML to prevent HTTP 500

--- apps/prestashop/client.py
+++ apps/prestashop/client.py
@@ -507,6 +507,7 @@ class PrestashopClient:
         self._set_text(product_node, "id_manufacturer", manufacturer_id)
         self._remove_node(product_node, "manufacturer_name")
         self._remove_node(product_node, "quantity")
+        self._remove_node(product_node, "id_default_combination")
         if tax_rules_group_id is not None:
             self._set_text(product_node, "id_tax_rules_group", str(tax_rules_group_id))
         self._set_text(product_node, "reference", product.reference)
